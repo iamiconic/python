fa08d5615e577bc187c9a8d881210c4c73774cd6
Chad Smith <chad.smith@ymail.com>
2023-11-21 14:02:24 +0800

    Remove test files after successful testing Change from copy files to move after successful testing Add a print statement after completion Add a README explaining the usage and future improvments

---
+++ file-renamer/README.md
@@ -0,0 +1,9 @@
+Program will take an organized container of files and sort them based of their created date.
+Organized into folders based on Year > Month 
+Useful to organize pictures
+
+Future improvements:
+    Using creation date for Windows (currently using modified date for compatibility with linux)
+    UI and customization (change input/output locations etc.)
+    Undo changes
+    Progress bar
--- file-renamer/file-renamer.py
+++ file-renamer/file-renamer.py
@@ -1,4 +1,4 @@
-import os #File and Directory operations
+import os #File and Directory function
 import shutil #Move/Copy functions
 from datetime import datetime #Date/Time manipulation
 
@@ -8,19 +8,21 @@ all_files = os.listdir(sort_dir)
 
 #Iterating over all files/folders in the Sort folder
 for file in all_files:
+
     file_path = os.path.join(sort_dir,file) #Joining the Sort directory with the file name to create a usable path
     if os.path.isfile(file_path): #If the iterable is not a file it will break and continue to next iterable
 
-        
-        mod_time = os.path.getmtime(file_path) #Fetching file modification date. Consider changing to creation date later.
+        mod_time = os.path.getmtime(file_path) #Fetching file modification date. 
         date = datetime.fromtimestamp(mod_time) #Formatting the time into a readable timestamp
 
         year = date.strftime("%Y") #Retrieving Year from timestamp
-        month = date.strftime("%B")#Retrieving Month from timestamp
+        month = date.strftime("%m - %B")#Retrieving Month from timestamp
         year_month_path = os.path.join(sort_dir,year,month) #Creating a Year/Month/file path
 
         #If the path is not already created it will be created
         if not os.path.exists(year_month_path): 
             os.makedirs(year_month_path)
 
-        shutil.copy2(file_path, os.path.join(year_month_path, file)) #Copy the file to its new folder. Use a move function later
+        shutil.move(file_path, os.path.join(year_month_path, file)) #Move the file to its new folder. 
+    
+print('Completed') #Print statment as confirmation operation is complete
